9e39bd1a9f767d71c94d260733e5af611026fbfb
Vantsevych <tatko.front@gmail.com>
2021-04-07 10:38:25 +0300

    create JSX React element

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 
-const h1 = React.createElement("h1",{
-  id:test
-},"Hello world");
+// const h1 = React.createElement("h1",{
+//   id:test
+// },"Hello world");
+
+
+
+// JSX
+const h1 = <h1 id="test2">Hello</h1>
 
 ReactDOM.render(h1,document.getElementById("root"))
